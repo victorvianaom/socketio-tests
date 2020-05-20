6d394a1918ba805dd9430e5206a44592b6db32a4
Victor Viana <victorvianaom@gmail.com>
2020-05-20 22:56:45 +0000

    ajustes iniciais no socket io, detalhes de css

--- server.js
+++ server.js
@@ -2,17 +2,21 @@ import express from "express"       //pra usar esses `import` eu tive que transf
 import http from "http"             //em um modulo pelo arquivo `package.json` com "type": "module"
 import socketio from "socket.io"    //caso contrario deveria ter usado require()
 
+const port = 3000
+
 const app = express() //creates an Express application
 const server = http.createServer(app)   // creates a server on my computer
                                         // 'server' listens for HTTP requests on a given port,
                                         // 'server' is now a Event Emitter also
 const sockets = socketio(server) // 'sockets' tbm is um Event Emitter
 
-app.use(express.static('public')); //faz a pasta `public` ser o foco do servidor
+app.use(express.static('public')) // faz a pasta `public` ser o foco do servidor
+
+sockets.on('connection', (socket) => {
+    console.log(`> Connected to socket ID: ${socket.id}`)
+})
 
 
-
-
-
-
-server.listen(3000)
+server.listen(port, () => {
+    console.log(`> Serving on port ${port}`)
+})
