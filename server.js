a6a9c5db92cd3e6d6c380278396c89fb77cbc607
Victor Viana <victorvianaom@gmail.com>
2020-05-21 19:27:31 +0000

    begin sockets programming

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express"       //pra usar esses `import` eu tive que transf
 import http from "http"             //em um modulo pelo arquivo `package.json` com "type": "module"
 import socketio from "socket.io"    //caso contrario deveria ter usado require()
 
-const port = 3000
+const port = 5000
 
 const app = express() //creates an Express application
 const server = http.createServer(app)   // creates a server on my computer
@@ -11,6 +11,8 @@ const server = http.createServer(app)   // creates a server on my computer
 const sockets = socketio(server) // 'sockets' tbm is um Event Emitter
 
 app.use(express.static('public')) // faz a pasta `public` ser o foco do servidor
+
+const messageBoard = {}
 
 sockets.on('connection', (socket) => {
     console.log(`> Connected to socket ID: ${socket.id}`)
